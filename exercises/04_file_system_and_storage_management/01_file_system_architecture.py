79c8b67564ff23a50d850a0450b9aa40b7101e88
mahdi <mxhdiqaim@gmail.com>
2025-04-24 17:30:49 +0100

    fileSystemAndStorageManagement: fileSystemAchitecture

---
+++ exercises/04_file_system_and_storage_management/01_file_system_architecture.py
@@ -0,0 +1,14 @@
+def calculate_block(file_size, block_size):
+    # Calculate the number of complete blocks
+    complete_blocks = file_size // block_size
+    # Determine if there is any remainder that requires an another block 
+    if file_size % block_size > 0:
+        complete_blocks += 1
+
+    return complete_blocks
+    
+# Example usage
+file_size = 1025  # in bytes
+block_size = 512  # in bytes
+blocks_needed = calculate_block(file_size, block_size)
+print(f"Number of blocks needed: {blocks_needed}")
